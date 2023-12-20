2c2144fb1146cc4823c4875b7f56a7437bff0061
KIM YOUNGMIN <122436515+Alex-Redlich@users.noreply.github.com>
2023-12-20 11:50:53 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 0 ms, Memory: 1124 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cc
@@ -0,0 +1,9 @@
+#include <stdio.h>
+ 
+int main(int argc, char const *argv[])
+{
+    int A, B;
+    scanf("%d %d", &A, &B);
+    printf("%s", (A > B) ? ">" : (A < B) ? "<" : "==");
+    return 0;
+}
